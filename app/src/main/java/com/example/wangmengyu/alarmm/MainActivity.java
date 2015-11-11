7203b7c79fe2a294de7288509fc5feeffeb4e48d
Auradreamer <wangmengyu@uosl15-23w0t32.oncampus.local>
2015-11-11 09:32:42 -0500

    create a thread

--- app/src/main/java/com/example/wangmengyu/alarmm/MainActivity.java
+++ app/src/main/java/com/example/wangmengyu/alarmm/MainActivity.java
@@ -32,6 +32,18 @@ public class MainActivity extends AppCompatActivity {
     Button start;
     Button stop;
     Button sync;
+    String error;
+    String e1;
+    String e2;
+    String err;
+
+    private DatabaseHelper myDB;
+    private URL url;
+    private HttpURLConnection urlConnection;
+    private OutputStream writer;
+    private InputStream listener;
+
+
 
 
 
@@ -59,14 +71,120 @@ public class MainActivity extends AppCompatActivity {
         stop();
         sync();
 
+    }
+
+    public void sendRequest() {
+                new Thread((new Runnable() {
+                    @Override
+                    public void run() {
 
 
+                        error = onPreExecute();
+                        err = send();
 
 
+                    }
+
+                })).start();
+    }
+
+    public String onPreExecute () {
+        myDB = new DatabaseHelper(MainActivity.this);
+        //dialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        try {
+            url = new URL("http://db.science.uoit.ca:9000");
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setReadTimeout(10000);
+            urlConnection.setConnectTimeout(15000);
+            urlConnection.setRequestMethod("POST");
+            urlConnection.setDoInput(true);
+            urlConnection.setDoOutput(true);
+            urlConnection.connect();
+            writer = new BufferedOutputStream(urlConnection.getOutputStream());
+            listener = new BufferedInputStream(urlConnection.getInputStream());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return e.toString();
+        }
+        return "";
+    }
+
+    protected String send(){
+
+        ScanRecord[] records = myDB.getAll();
+        int totalRecords = records.length;
+        String postData = "";
+        String response = "";
+        byte[] contents = new byte[1024];
+        int bytesRead = 0;
+        int currentRecord = 1;
 
 
+        //Kill task if there are no records
+        if (totalRecords == 0) {
+            e1 = "Nothing in the local database to send";
+            return e1;
+           // Toast.makeText(getApplicationContext(), "Nothing in the local database to send.", Toast.LENGTH_LONG).show();
+           // return null;
+        }
+
+        //send each record to server
+        for (ScanRecord r : records) {
+
+            //create the post data in a string
+            try {
+                postData = URLEncoder.encode("id", "UTF-8") + "=" + URLEncoder.encode(r.idToString(), "UTF-8")
+                        + "&" + URLEncoder.encode("timestamp", "UTF-8") + "=" + URLEncoder.encode(r.timestamp, "UTF-8")
+                        + "&" + URLEncoder.encode("ssid", "UTF-8") + "=" + URLEncoder.encode(r.ssid, "UTF-8")
+                        + "&" + URLEncoder.encode("level", "UTF-8") + "=" + URLEncoder.encode(r.levelToString(), "UTF-8");
+                       // + "&" + URLEncoder.encode("android_id", "UTF-8") + "=" + URLEncoder.encode(android_id, "UTF-8");
+            } catch (Exception e) {
+                //oast.makeText(getApplicationContext(), "Encoding error: " + e.toString(), Toast.LENGTH_LONG).show();
+                return e.toString();
+            }
 
 
+            //send data
+            try {
+                writer.write(postData.getBytes());
+                writer.flush();
+
+                while ((bytesRead = listener.read(contents)) != -1) {
+                    response += new String(contents, 0, bytesRead);
+                }
+
+                if (!response.isEmpty()) {
+                   // Toast.makeText(getApplicationContext(), "Problem sending data, received response from server: "
+                     //       + response, Toast.LENGTH_LONG).show();
+                    e2 = "Problem sending data, received response from server: " + response;
+                    return e2;
+                }
+
+                //remove record from local database
+                myDB.delete(r.id);
+
+
+            } catch (Exception e) {
+                //Toast.makeText(getApplicationContext(), "Problem sending data: " + e.toString(), Toast.LENGTH_LONG).show();
+                return e.toString();
+               // return null;
+            } //end try to send data
+
+            //update progress bar
+           // dialog.setProgress((int) Math.ceil(currentRecord / totalRecords));
+            currentRecord++;
+
+        } //for each r in record
+
+        return null;
+    }
+
+    protected void onProgressUpdate(String... progress) {
+
+    }
+
+    protected void onPostExecute(Void unused) {
+        //dialog.dismiss();
     }
 
     public void start() {
@@ -80,7 +198,7 @@ public class MainActivity extends AppCompatActivity {
                         switch (view.getId()) {
                             case R.id.startbtn:
                                 //startService(intent);
-                                PollingUtils.startPollingService(MainActivity.this, 60, MyService.class, MyService.ACTION);
+                                PollingUtils.startPollingService(MainActivity.this, 300 , MyService.class, MyService.ACTION);
                                 break;
                             default:
                                 break;
@@ -131,16 +249,22 @@ public class MainActivity extends AppCompatActivity {
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        UploadTask task = new UploadTask();
-                        task.execute();
-                        /*String error = myHttpHelper.createConnection();
+
+                        sendRequest();
+
                         if (error != null) {
                             Toast.makeText(MainActivity.this, "Connection error\n" + error, Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(MainActivity.this, "Connection success", Toast.LENGTH_SHORT).show();
                         }
 
+                        if (err != null) {
+                            Toast.makeText(MainActivity.this, "error\n" + err, Toast.LENGTH_SHORT).show();
+                        } else {
 
-                        myHttpHelper.removeConnection();
-                        */
+                            Toast.makeText(MainActivity.this, "send success", Toast.LENGTH_SHORT).show();
+
+                        }
 
 
                     }
@@ -148,9 +272,6 @@ public class MainActivity extends AppCompatActivity {
 
 
         );
-
-
-
 
     }
 
@@ -179,109 +300,5 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private class UploadTask extends AsyncTask<Void, Void, Void> {
 
-        private DatabaseHelper myDB;
-        private URL url;
-        private HttpURLConnection urlConnection;
-        private OutputStream writer;
-        private InputStream listener;
-        private ProgressDialog dialog = new ProgressDialog(MainActivity.this);
-        private String android_id = Secure.getString(MainActivity.this.getContentResolver(), Secure.ANDROID_ID);
-
-
-        public void onPreExecute() {
-            myDB = new DatabaseHelper(MainActivity.this);
-            dialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
-            try {
-                url = new URL("http://db.science.uoit.ca:9000");
-                urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.setReadTimeout(10000);
-                urlConnection.setConnectTimeout(15000);
-                urlConnection.setRequestMethod("POST");
-                urlConnection.setDoInput(true);
-                urlConnection.setDoOutput(true);
-                writer = new BufferedOutputStream(urlConnection.getOutputStream());
-                listener = new BufferedInputStream(urlConnection.getInputStream());
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-        @Override
-        protected Void doInBackground(Void... params) {
-
-            ScanRecord[] records = myDB.getAll();
-            int totalRecords = records.length;
-            String postData = "";
-            String response = "";
-            byte[] contents = new byte[1024];
-            int bytesRead = 0;
-            int currentRecord = 1;
-
-
-            //Kill task if there are no records
-            if (totalRecords == 0) {
-                Toast.makeText(getApplicationContext(), "Nothing in the local database to send.", Toast.LENGTH_LONG).show();
-                return null;
-            }
-
-            //send each record to server
-            for (ScanRecord r : records){
-
-                //create the post data in a string
-                try {
-                    postData = URLEncoder.encode("id", "UTF-8") + "=" + URLEncoder.encode(r.idToString(), "UTF-8")
-                            + "&" + URLEncoder.encode("timestamp", "UTF-8") + "=" + URLEncoder.encode(r.timestamp, "UTF-8")
-                            + "&" + URLEncoder.encode("ssid", "UTF-8") + "=" + URLEncoder.encode(r.ssid, "UTF-8")
-                            + "&" + URLEncoder.encode("level", "UTF-8") + "=" + URLEncoder.encode(r.levelToString(), "UTF-8")
-                            + "&" + URLEncoder.encode("android_id", "UTF-8") + "=" + URLEncoder.encode(android_id, "UTF-8");
-                } catch (Exception e) {
-                    Toast.makeText(getApplicationContext(), "Encoding error: "+e.toString(), Toast.LENGTH_LONG).show();
-                    return null;
-                }
-
-
-                //send data
-                try {
-                    writer.write(postData.getBytes());
-                    writer.flush();
-
-                    while( (bytesRead = listener.read(contents)) != -1) {
-                        response+= new String(contents, 0, bytesRead);
-                    }
-
-                    if (!response.isEmpty()) {
-                        Toast.makeText(getApplicationContext(), "Problem sending data, received response from server: "
-                                +response, Toast.LENGTH_LONG).show();
-                        return null;
-                    }
-
-                    //remove record from local database
-                    myDB.delete(r.id);
-
-
-                } catch (Exception e) {
-                    Toast.makeText(getApplicationContext(), "Problem sending data: "+e.toString(), Toast.LENGTH_LONG).show();
-                    return null;
-                } //end try to send data
-
-                //update progress bar
-                dialog.setProgress((int)Math.ceil(currentRecord/totalRecords));
-                currentRecord++;
-
-            } //for each r in record
-
-            return null;
-        }
-
-        protected void onProgressUpdate(String... progress) {
-
-        }
-
-        protected void onPostExecute(Void unused) {
-            dialog.dismiss();
-        }
-
-    } //end UploadTask
 }
